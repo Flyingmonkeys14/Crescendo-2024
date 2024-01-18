b08cedafaffc0bbe67cea6e77a58099c54ad342f
6121 Robovikes <35512043+6121Robovikes@users.noreply.github.com>
2024-01-18 14:56:16 -0500

    finished subsystems

--- src/main/java/frc/robot/commands/Te;leopSwerve.java
+++ src/main/java/frc/robot/commands/Te;leopSwerve.java
@@ -23,7 +23,7 @@ public class TeleopSwerve extends CommandBase {
    private BooleanSupplier robotCentricSup;
   
   public TeleopSwerve(Swerve s_Swerve, DoubleSupplier translationSup, DoubleSupplier strafeSup, DoubleSupplier rotationSup, BooleanSupplier robotCentricSup) {
-    this.s_Swerve;
+    this.s_Swerve = s_Swerve;
     addRequirements(s_Swerve);
   
   this.translationSup = translationSup;
